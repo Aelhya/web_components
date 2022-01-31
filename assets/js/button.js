d6e5ce0a408c1a1462c003f07b37600a30d32912
Adélaïde LOUIS <Adlade LOUIS@info-p-lp-48.iut.bx1>
2022-01-31 15:32:30 +0100

    Ajout du site

---
+++ assets/js/button.js
@@ -0,0 +1,57 @@
+const templateBtn = document.createElement("template");
+templateBtn.innerHTML = `
+<style>
+.button {
+    display: inline-block;
+    border-radius: 5%;
+    background-color: #f4511e;
+    border: none;
+    color: #FFFFFF;
+    text-align: center;
+    font-size: 12px;
+    padding: 1%;
+    width: 15%;
+    transition: all 0.5s;
+    cursor: pointer;
+    margin: 5px;
+    vertical-align: middle;
+    align-self : center;
+  }
+  
+  .button span {
+    cursor: pointer;
+    display: inline-block;
+    position: relative;
+    transition: 0.5s;
+  }
+  
+  .button span:after {
+    content: '⇾';
+    position: absolute;
+    opacity: 0;
+    top: 0;
+    right: -30px;
+    transition: 0.5s;
+  }
+  
+  .button:hover span {
+    padding-right: 25px;
+  }
+  
+  .button:hover span:after {
+    opacity: 1;
+    right: 0;
+  }
+</style>
+<a class="button" href="web_component.html">
+    <span>Page présentation</span>
+</a>`;
+
+class MyButton extends HTMLElement {
+    constructor() {
+        super();
+        this.attachShadow({ mode: 'open' });
+        this.shadowRoot.appendChild(templateBtn.content);
+    }
+}
+customElements.define('my-button', MyButton);
---
+++ assets/js/layout.js
@@ -0,0 +1,82 @@
+const template = document.createElement("template");
+template.innerHTML = `
+<style>
+nav ul {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+    background-color: #333;
+}
+
+nav li {
+    float: left;
+}
+
+nav li a {
+    display: block;
+    color: white;
+    text-align: center;
+    padding: 14px 16px;
+    text-decoration: none;
+}
+
+nav li a:hover {
+    background-color: #111;
+}
+</style>
+
+
+<nav>
+    <div id="navbarText">
+        <ul>
+            <li><a href="index.html">Accueil</a>
+            </li>
+            <li><a href="web_component.html">Introduction aux composants Web</a></li>
+            <li><a href="tutorials.html">Tutoriels</a></li>
+            <li><a href="sources.html">Sources</a></li>
+        </ul>
+    </div>
+</nav>
+`;
+
+class MyNav extends HTMLElement {
+    constructor() {
+        super();
+        this.attachShadow({ mode: 'open' });
+        this.shadowRoot.appendChild(template.content);
+    }
+
+}
+customElements.define('my-nav', MyNav);
+
+
+const templateFooter = document.createElement("template"); // création de la balise <template>
+templateFooter.innerHTML = `
+<style>
+footer {
+    background-color : #333;
+    color : white;
+    display : flex;
+    justify-content: center;
+    align-self : center;
+    font-size : 9px;
+    width : 100%;
+}
+footer div {
+    margin : 1em;
+}
+</style>
+<footer>
+    <div>Veille technologique</div>
+    <div>LOUIS Adélaïde</div>
+    <div>LP DAWIN A</div>
+</footer>`; // contenu de la balise template
+class MyFooter extends HTMLElement {
+    constructor() {
+        super();
+        this.attachShadow({ mode: 'open' }); // autorisation de modifier le shadow DOM
+        this.shadowRoot.appendChild(templateFooter.content); // Mettre le contenu du template dans le shadow DOM
+    }
+}
+customElements.define('my-footer', MyFooter); // définir le nom du composant
